3a198617295b2608a452fec41bbe107ca36584a5
rupam-04 <rpmdey2004@gmail.com>
2023-10-26 18:57:40 +0530

    completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+
 module.exports = {
    entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
    output: {
@@ -6,7 +7,7 @@ module.exports = {
       filename: 'build.js' // The final file will be created in dist/build.js
    },
    module: {
-      rules: [{
+      loaders: [{
          test: /\.css$/, // To load the css in react
          use: ['style-loader', 'css-loader'],
          include: /src/
@@ -17,6 +18,9 @@ module.exports = {
          query: {
             presets: ['es2015', 'react', 'stage-2']
          }
+      }, {
+         test: /\.json$/, // To load the json files
+         loader: 'json-loader'
       }]
    }
 }
